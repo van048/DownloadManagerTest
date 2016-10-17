c461318714f4d48c37919f3de42f0f55fdcd9582
van048 <yybbenyang@gmail.com>
2016-10-17 15:45:49 +0800

    compatible 7.0

--- app/src/main/java/cn/ben/downloadmanagertest/service/DownloadServiceCompatibleWithN.java
+++ app/src/main/java/cn/ben/downloadmanagertest/service/DownloadServiceCompatibleWithN.java
@@ -8,9 +8,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 
 import com.tbruyelle.rxpermissions.RxPermissions;
 
@@ -38,7 +40,11 @@ public class DownloadServiceCompatibleWithN extends Service {
         receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                install(context);
+//              The package installer only supports content schemes starting on Android 7.0.
+//              Prior to that — and despite documentation to the contrary — the package installer only supports file schemes.
+//              You will need to set the Uri on your Intent differently based on whether you are running on Android 7.0+ or not, such as by branching on Build.VERSION.SDK_INT.
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) install(context);
+                else installCompatible(context);
                 stopSelf();
             }
         };
@@ -68,6 +74,7 @@ public class DownloadServiceCompatibleWithN extends Service {
         enqueue = dm.enqueue(request);
     }
 
+    @SuppressWarnings("unused")
     private void install(Context context) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         //沒有在activity環境下啟動
@@ -78,6 +85,20 @@ public class DownloadServiceCompatibleWithN extends Service {
         //“私有目录被限制访问“ 是指在Android7.0中为了提高私有文件的安全性，面向 Android N 或更高版本的应用私有目录将被限制访问。这点类似iOS的沙盒机制。
         //" StrictMode API 政策" 是指禁止向你的应用外公开 file:// URI。 如果一项包含文件 file:// URI类型 的 Intent 离开你的应用，应用失败，并出现 FileUriExposedException 异常。
         intent.setDataAndType(Uri.fromFile(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "1.apk")), "application/vnd.android.package-archive");// TODO: 2016/10/17
+        context.startActivity(intent);
+    }
+
+    //将之前Uri改成了有FileProvider创建一个content类型的Uri
+    private void installCompatible(Context context) {
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "1.apk");
+        Uri apkUri = FileProvider.getUriForFile(context, "cn.ben.downloadmanagertest.fileprovider", file);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        //沒有在activity環境下啟動
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        // TODO: 2016/10/17 對目標應用臨時授權該Uri所代表的文件
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setDataAndType(apkUri, "application/vnd.android.package-archive");// TODO: 2016/10/17
         context.startActivity(intent);
     }
 
